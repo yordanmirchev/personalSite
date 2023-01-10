3fd87239c3253b2d29372cf1e0449b97f5dc32d0
Yordan Mirchev <ymm@example.com>
2023-01-10 18:46:22 +0200

    Added exercise and quiz brain game.

---
+++ quiz-game-start/data.py
@@ -0,0 +1,31 @@
+# We can get more data from : https://opentdb.com/api_config.php
+## Example : https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean
+
+question_data = [
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The sum of all the numbers on a roulette wheel is 666.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+                                       "question": "Furby was released in 1998.", "correct_answer": "True",
+                                       "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The bikini is named after the &quot;Bikini Atoll&quot;, an island where the United States conducted tests on atomic bombs.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "Cucumbers are usually more than 90% water.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+                                       "question": "You are allowed to sell your soul on eBay.",
+                                       "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "Kissing someone for one minute burns about 2 calories.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+                                       "question": "Fast food restaurant chains Carl&#039;s Jr. and Hardee&#039;s are owned by the same company.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "&quot;Santa Claus&quot; is a variety of melon.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+                                       "question": "The British organisation CAMRA stands for The Campaign for Real Ale.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium",
+     "question": "The commercial UK channel ITV stands for &quot;International Television&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}
+    ]
