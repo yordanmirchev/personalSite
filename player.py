bc590c05bbc5c685e35da386a23f1eece14f6180
Yordan Mirchev <ymm@example.com>
2023-01-20 22:35:37 +0200

    Car game.

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-from constants import TURTLE_STARTING_X, TURTLE_STARTING_Y
+from constants import TURTLE_STARTING_X, TURTLE_STARTING_Y, TURTLE_STEP, SCREEN_HEIGHT, BUFFER
 
 
 class Player(Turtle):
@@ -11,5 +11,15 @@ class Player(Turtle):
         self.color("black")
         self.penup()
         self.setheading(90)
-        self.goto(TURTLE_STARTING_X, TURTLE_STARTING_Y)
+        self.reset_position()
         self.showturtle()
+
+
+    def reset_position(self):
+        self.goto(TURTLE_STARTING_X, TURTLE_STARTING_Y)
+
+    def up(self):
+        self.forward(TURTLE_STEP)
+
+    def is_at_finish(self):
+       return self.ycor() >= int(SCREEN_HEIGHT / 2) - BUFFER
