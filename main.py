8820d9a5bafaf6a567c4fed34dff0b41b2ef169b
Yordan Mirchev <ymm@example.com>
2023-01-21 16:44:05 +0200

    Added relative and absolute paths tests.

--- main.py
+++ main.py
@@ -25,3 +25,11 @@ with open("new_file.txt", mode='w') as file:
     file.write("\nNew file\n")
 
 file_read_with("new_file.txt")
+
+with open("/Users/yordan.mirchev/Desktop/sent_file.xml") as file:
+    print(file.read())
+
+print("\nUse relative to project path: \n")
+with open("../../Desktop/sent_file.xml") as file:
+    print(file.read())
+
