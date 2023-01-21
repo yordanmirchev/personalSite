b1d3aa3bf57c806410fadda47d386866c29fd91c
Yordan Mirchev <ymm@example.com>
2023-01-21 15:22:46 +0200

    manage files with with

--- main.py
+++ main.py
@@ -2,3 +2,26 @@ file = open("my_file.txt")
 contents = file.read()
 print(contents)
 file.close()
+
+def file_read_with(file_to_read):
+    with open(file_to_read) as file:
+        contents = file.read()
+        print(contents)
+
+# write
+with open("my_file.txt", mode='w') as file:
+    file.write("Text replaced\nSecond line.\nThird line replaced.")
+
+
+# append
+with open("my_file.txt", mode='a') as file:
+    file.write("\ntext appended.")
+
+file_read_with("my_file.txt")
+
+
+# if file not exists it will be created if in write mode
+with open("new_file.txt", mode='w') as file:
+    file.write("\nNew file\n")
+
+file_read_with("new_file.txt")
