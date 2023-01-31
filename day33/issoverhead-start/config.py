357256bc360f49629bdee1224b2816d9f2e2eaf3
Yordan Mirchev <ymm@example.com>
2023-01-31 23:45:58 +0200

    Added iss checker and notifier with basic APIs.

---
+++ day33/issoverhead-start/config.py
@@ -0,0 +1,14 @@
+MY_LAT = "42.697708"
+MY_LONG = "23.321867"
+MARGIN = 5
+WAIT_TIME = 60
+
+
+parameters = {
+    "lat": {MY_LAT},
+    "lng": {MY_LONG},
+    "formatted": 0
+}
+
+my_email = "jorangel1234567@gmail.com"
+my_password = "tbqkchvwfoqppsna"
