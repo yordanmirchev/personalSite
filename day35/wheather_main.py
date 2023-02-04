2e5ba02586cffd2711c8d3ef0f6d9ed2bb3acae6
Yordan Mirchev <ymm@example.com>
2023-02-04 09:58:51 +0200

    Modified wheather not to send sms.

--- day35/wheather_main.py
+++ day35/wheather_main.py
@@ -11,8 +11,6 @@ OMW_API_KEY= "5a99b9c852d3782788fd305728355c33"
 URL = "https://api.openweathermap.org/data/2.5/forecast?"
 time_to_rain = ""
 
-print(OMW_API_KEY)
-
 params = {
     "lat": LAT,
     "lon": LON,
@@ -36,22 +34,25 @@ def is_raining_in_12_hours():
         if int(id) < 700 :
             global time_to_rain
             time_to_rain = tm
+            print(f"\nIt will rain arround {time_to_rain}.")
             return True
 
-
+    print("\nIt won't rain in the next 12 hours.")
     return False
 
-if (is_raining_in_12_hours()):
-    # Set environment variables for your credentials
-    # Read more at http://twil.io/secure
-    account_sid = "ACe28f22bf4e973c0679b209ea2ed3b50a"
-    auth_token = "536ae0b5a51ba35e415538a7378c2f81"
-    client = Client(account_sid, auth_token)
-
-    message = client.messages.create(
-        body = f"It will rain at {time_to_rain} ☂️☂️",
-        from_="+19704447472",
-        to="+359878135153"
-    )
-
-    print(f"{message.status} : {message.sid}")
+is_raining_in_12_hours()
+#
+# if (is_raining_in_12_hours()):
+#     # Set environment variables for your credentials
+#     # Read more at http://twil.io/secure
+#     account_sid = "ACe28f22bf4e973c0679b209ea2ed3b50a"
+#     auth_token = "536ae0b5a51ba35e415538a7378c2f81"
+#     client = Client(account_sid, auth_token)
+#
+#     message = client.messages.create(
+#         body = f"It will rain at {time_to_rain} ☂️☂️",
+#         from_="+19704447472",
+#         to="+359878135153"
+#     )
+#
+#     print(f"{message.status} : {message.sid}")
