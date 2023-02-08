f47025eff532876f1d90980a4bd20f77f3cfc991
Yordan Mirchev <ymm@example.com>
2023-02-08 13:55:05 +0200

    Added notification_manager

--- day39/flight-deals-start/notification_manager.py
+++ day39/flight-deals-start/notification_manager.py
@@ -1,3 +1,9 @@
+from _commons.commons import send_mail
+
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def send_email(self, message):
+        send_mail(recepient="jorangel1234567@gmail.com",
+                  subject=f"Flight offer",
+                  message= message)
